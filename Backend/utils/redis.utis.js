1d517b33e996c77218ccde52272f47a50e0d3998
parth-1711 <parthirache8@gmail.com>
2024-05-02 02:06:58 +0530

    fix: changed redis host-name

--- Backend/utils/redis.utis.js
+++ Backend/utils/redis.utis.js
@@ -1,7 +1,7 @@
 const Redis = require('ioredis');
 
 const redisClient = new Redis({
-    host: 'redis-server', // Redis server address
+    host: process.env.REDIS_HOST||'redis-server', // Redis server address
     port: 6379,        // Redis server port
   });
   
